504bf9574a5bfa9af4bd7791899dadc7787d7bf0
AbnerCestari <abner_cestari@hotmail.com>
2023-01-30 00:39:35 -0300

    added error exceptions in order to minimize program errors

--- Burger Palace.py
+++ Burger Palace.py
@@ -2,6 +2,7 @@
 #random number generator
 #while loops
 #code to test new and add content learned in Python language as I study
+#Error exceptions
 
 #The idea is to simulate customer service in a burger joint where you are greeted, given options and place your order
 
@@ -19,27 +20,46 @@ while True:
     if (choice1 == 'yes') or (choice1 == 'yeah') or (choice1 == 'y'): #logical operador "or" (in case one of the conditions is true, then the outcome will be True, in case one or more of the conditions is false, then the outcome will be False)
 
         whichburger = input ('Which burger would you like to order? Create anything and tell me. ')
-
-        burgers = int (input ('How many {} would you like, 1, 2, 3, 54? '.format(whichburger))) #converting acquired values through input to integer
+        while True:
+            try:
+                burgers = int (input ('How many {} would you like, 1, 2, 3, 54? '.format(whichburger))) #converting acquired values through input to integer
+            except ValueError:
+                print ('Please type a number: ')
+                continue
+            else:
+                break
 
         if burgers == 0:
             print ('Oh well, no burgers then')
 
-        elif burgers >= 1:
+        elif burgers >= 0:
             patty = input ('How would you like the patty? For example, rare, medium rare, well done... any way you want! ')
             print ('\nOk, {} {} {} coming up.\n'.format(burgers,patty,whichburger))
     else:
         burgers = 0
         print ('No burger, no problem.\n')
 
-    drink = input ("Would you like any beverages? ").lower()
-    if (drink == 'yes') or (drink == 'yeah') or (drink == 'y'):
-        bev = input ("Which beverage? We have all you can imagine, just name it! ")
-        bevammount = int (input ("How many {} would you like, 1, 2, 3? ".format(bev)))
+    while True:
+        drink = input ("Would you like any beverages? ").lower()
+        if (drink == 'yes') or (drink == 'yeah') or (drink == 'y'):
+            bev = input ("Which beverage? We have all you can imagine, just name it! ")
 
-    else:
-        bevammount = 0
-        print ("Ok, no drinks.")
+            while True:
+                try:
+                    bevammount = int (input ("How many {} would you like, 1, 2, 3? ".format(bev)))
+                except ValueError:
+                    print ('Please type a number: ')
+                    continue
+                else:
+                    break
+            if bevammount == 0:
+                print ('Oh well, no drinks then')
+            elif bevammount >= 0:
+                print ('Got it, {} {} coming up!'.format(bevammount,bev))
+                break
+        elif drink == 'no':
+            bevammount == 0
+            break
 
     bevprice = 7.5
     burgerprice = 15.9
@@ -53,7 +73,7 @@ while True:
     print ('\nAlright. The order total is ${:.2f}'.format(total))
     break
 
-#payment choices
+#payment
 payment = input ("Would you like to pay by cash or card? ").lower() #lots of nested conditionals
 if payment != "card":
     cash = float (input ("How much in cash are you paying with? $"))
@@ -79,7 +99,7 @@ if (bevammount >= 1) and (burgers == 0):
     print ("Here you go, {} {}!\n". format (bevammount,bev))
     print ("You're always welcome back here at Buger Palace! Bye!")
     quit ()
-    
+
 elif (bevammount >= 1) and (burgers >= 1):
     print ("Kitchen, make {} {}, client's patty is {}!! Don't forget {} {}!!\n". format (burgers,whichburger,patty,bevammount,bev))
     
@@ -89,3 +109,4 @@ elif (bevammount == 0) and (burgers >= 1):
 print ("""Ok, your table number is {}. 
 As soon as your order is ready, we will serve it on your table.\n
 Thank you for choosing Burger Palace!""".format(table)) #three double quotation marks """ allows the string to be printed in several lines
+
